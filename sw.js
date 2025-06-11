10faf2696491a73614d34dd29d25aeb0383f6143
saleh1232112225222 <155776984+saleh1232112225222@users.noreply.github.com>
2025-06-11 16:02:28 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install', e => { e.waitUntil(caches.open('v1').then(cache => { return cache.addAll(['/', '/index.html', '/style.css', '/script.js', '/manifest.json', '/icon-192.png', '/icon-512.png']); })); }); self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(response => { return response || fetch(e.request); })); });
